8b1f01dfa2d2d4274ea007f930010593467c7a93
datsuns <the.skeleton7@gmail.com>
2012-06-24 15:23:56 +0900

    多少のリファクタリングを実施

--- jihanki_spec.rb
+++ jihanki_spec.rb
@@ -3,8 +3,16 @@
 require './jihanki'
 require './money'
 describe Jihanki do
+	before(:each) do 
+		@jihanki = Jihanki.new
+	end
+
+	after(:each) do 
+	end
+
+
 	it "get_total() with initial state " do
-		Jihanki.new.get_total.should eq(0)
+		@jihanki.get_total.should eq(0)
 	end
 
 	it "money" do 
@@ -13,57 +21,52 @@ describe Jihanki do
 	end
 
 	it "投入 10円を入れたら10円取得できる" do
-		ope = Jihanki.new
-		ope.insert(Money::YEN_10)
-		ope.get_total.should  eq(10)
+		@jihanki.insert(Money::YEN_10)
+		@jihanki.get_total.should  eq(10)
 	end
 
 	it "投入 50円を入れたら10円取得できる" do
-		ope = Jihanki.new
-		ope.insert(Money::YEN_50)
-		ope.get_total.should  eq(50)
+		@jihanki.insert(Money::YEN_50)
+		@jihanki.get_total.should  eq(50)
 	end
 
 	it "投入 二回投入して合計を取得できる" do
-		ope = Jihanki.new
-		ope.insert(Money::YEN_50)
-		ope.insert(Money::YEN_1000)
-		ope.get_total.should  eq(1050)
+		@jihanki.insert(Money::YEN_50)
+		@jihanki.insert(Money::YEN_1000)
+		@jihanki.get_total.should  eq(1050)
 	end
 
 	it "投入せずに払い戻しで0円を返す" do
-		ope = Jihanki.new
-		ope.payback.should eq(0)
-		ope.get_total.should eq(0)
+		@jihanki.payback.should eq(0)
+		@jihanki.get_total.should eq(0)
 	end
 
 	it "10円投入して払い戻しで10円を返す" do
-		ope = Jihanki.new
-		ope.insert(Money::YEN_10)
-		ope.payback.should eq(10)
-		ope.get_total.should eq(0)
+		@jihanki.insert(Money::YEN_10)
+		@jihanki.payback.should eq(10)
+		@jihanki.get_total.should eq(0)
 	end
 
 	it "10円,100円,1000円投入して払い戻しで1110円を返す" do
-		ope = Jihanki.new
-		ope.insert(Money::YEN_10)
-		ope.insert(Money::YEN_100)
-		ope.insert(Money::YEN_1000)
-		ope.payback.should eq(1110)
-		ope.get_total.should eq(0)
+		@jihanki.insert(Money::YEN_10)
+		@jihanki.insert(Money::YEN_100)
+		@jihanki.insert(Money::YEN_1000)
+		@jihanki.payback.should eq(1110)
+		@jihanki.get_total.should eq(0)
 	end
 
 	it "投入して払い戻しの3回繰り返し" do
-		ope = Jihanki.new
-		ope.insert(Money::YEN_10)
-		ope.payback.should eq(10)
-		ope.get_total.should eq(0)
-		ope.insert(Money::YEN_100)
-		ope.payback.should eq(100)
-		ope.get_total.should eq(0)
-		ope.insert(Money::YEN_1000)
-		ope.payback.should eq(1000)
-		ope.get_total.should eq(0)
+		@jihanki.insert(Money::YEN_10)
+		@jihanki.payback.should eq(10)
+		@jihanki.get_total.should eq(0)
+
+		@jihanki.insert(Money::YEN_100)
+		@jihanki.payback.should eq(100)
+		@jihanki.get_total.should eq(0)
+
+		@jihanki.insert(Money::YEN_1000)
+		@jihanki.payback.should eq(1000)
+		@jihanki.get_total.should eq(0)
 	end
 end
 
