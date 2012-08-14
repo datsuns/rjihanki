009d7666a7682f8db6670cd8cb865de911796a2c
datsuns <the.skeleton7@gmail.com>
2012-08-15 01:48:32 +0900

    不要なカッコを全部取っ払った

--- jihanki_spec.rb
+++ jihanki_spec.rb
@@ -36,49 +36,49 @@ describe Jihanki do
 
   context '初期状態' do
     it_should_behave_like '存在しない在庫を取得するとnilを返す' 
-    its(:get_total){ should eq(0) }
-    its(:payback){ should eq(0) }
+    its(:get_total){ should eq 0 }
+    its(:payback){ should eq 0 }
 
     it 'コーラの在庫が5本ある' do
-      cola = subject.get_stock(Juice::COLA)
-      cola.num.should eq(5)
-      cola.juice.name.should eq(Juice::COLA)
-      cola.juice.price.should eq(120)
+      cola = subject.get_stock Juice::COLA
+      cola.num.should eq 5
+      cola.juice.name.should eq Juice::COLA
+      cola.juice.price.should eq 120
     end
 
     it 'レッドブルの在庫が5本ある' do
-      redbull = subject.get_stock(Juice::REDBULL)
-      redbull.num.should eq(5)
-      redbull.juice.name.should eq(Juice::REDBULL)
-      redbull.juice.price.should eq(200)
+      redbull = subject.get_stock Juice::REDBULL
+      redbull.num.should eq 5
+      redbull.juice.name.should eq Juice::REDBULL
+      redbull.juice.price.should eq 200
     end
 
     it '水の在庫が5本ある' do
-      water = subject.get_stock(Juice::WATER)
-      water.num.should eq(5)
-      water.juice.name.should eq(Juice::WATER)
-      water.juice.price.should eq(100)
+      water = subject.get_stock Juice::WATER
+      water.num.should eq 5
+      water.juice.name.should eq Juice::WATER
+      water.juice.price.should eq 100
     end
 
     it '投入と払い戻しを繰り返せる' do
-      subject.insert(Money::YEN_10)
-      subject.payback.should eq(10)
-      subject.get_total.should eq(0)
+      subject.insert Money::YEN_10
+      subject.payback.should eq 10
+      subject.get_total.should eq 0
 
-      subject.insert(Money::YEN_100)
-      subject.payback.should eq(100)
-      subject.get_total.should eq(0)
+      subject.insert Money::YEN_100
+      subject.payback.should eq 100
+      subject.get_total.should eq 0
 
-      subject.insert(Money::YEN_1000)
-      subject.payback.should eq(1000)
-      subject.get_total.should eq(0)
+      subject.insert Money::YEN_1000
+      subject.payback.should eq 1000
+      subject.get_total.should eq 0
     end
   end
 
   context '10円投入状態' do
     subject do
       jihanki = Jihanki.new
-      jihanki.insert(Money::YEN_10)
+      jihanki.insert Money::YEN_10
       jihanki
     end
 
@@ -86,28 +86,28 @@ describe Jihanki do
     its(:get_total){ should eq 10 }
 
     it "払い戻しで10円を返す" do
-      subject.payback.should eq(10)
-      subject.get_total.should eq(0)
+      subject.payback.should eq 10
+      subject.get_total.should eq 0
     end
   end
 
   context '50円投入状態' do
     subject do
       jihanki = Jihanki.new
-      jihanki.insert(Money::YEN_50)
+      jihanki.insert Money::YEN_50
       jihanki
     end
 
     it_should_behave_like '存在しない在庫を取得するとnilを返す' 
-    its(:get_total){ should eq(50) }
+    its(:get_total){ should eq 50 }
   end
 
   context '120円投入状態' do
     subject do
       jihanki = Jihanki.new
-      jihanki.insert(Money::YEN_100)
-      jihanki.insert(Money::YEN_10)
-      jihanki.insert(Money::YEN_10)
+      jihanki.insert Money::YEN_100
+      jihanki.insert Money::YEN_10
+      jihanki.insert Money::YEN_10
       jihanki
     end
 
@@ -120,23 +120,23 @@ describe Jihanki do
   context '想定外のお金を投入するとそのまま戻ってくる' do
     it_should_behave_like '存在しない在庫を取得するとnilを返す' 
     context '1円を投入' do
-      it{ subject.insert(1).should eq(1) }
+      it{ subject.insert(1).should eq 1 }
     end
 
     context '5円を投入' do
-      it{ subject.insert(5).should eq(5) }
+      it{ subject.insert(5).should eq 5 }
     end
 
     context '5000円を投入' do
-      it{ subject.insert(5000).should eq(5000) }
+      it{ subject.insert(5000).should eq 5000 }
     end
   end
 
   context '二回で合計1050円投入する' do
     subject{
       j = Jihanki.new
-      j.insert(Money::YEN_50)
-      j.insert(Money::YEN_1000)
+      j.insert Money::YEN_50
+      j.insert Money::YEN_1000
       j
     }
     it_should_behave_like '存在しない在庫を取得するとnilを返す' 
@@ -145,21 +145,21 @@ describe Jihanki do
 
 
 	it "10円,100円,1000円投入して払い戻しで1110円を返す" do
-		subject.insert(Money::YEN_10)
-		subject.insert(Money::YEN_100)
-		subject.insert(Money::YEN_1000)
-		subject.payback.should eq(1110)
-		subject.get_total.should eq(0)
+		subject.insert Money::YEN_10
+		subject.insert Money::YEN_100
+		subject.insert Money::YEN_1000
+		subject.payback.should eq 1110
+		subject.get_total.should eq 0
 	end
 
 
 
 
 	it "120円投入するとコーラが買える事がわかる" do
-		subject.insert(Money::YEN_100)
-		subject.insert(Money::YEN_10)
-		subject.insert(Money::YEN_10)
-		subject.buyable?(Juice::COLA).should eq(true)
+		subject.insert Money::YEN_100
+		subject.insert Money::YEN_10
+		subject.insert Money::YEN_10
+		subject.buyable?(Juice::COLA).should eq true
 	end
 
 	it "90円投入ではコーラが買えないことがわかる" do
@@ -168,28 +168,28 @@ describe Jihanki do
 		subject.insert Money::YEN_10
 		subject.insert Money::YEN_10
 		subject.insert Money::YEN_50
-		subject.buyable?(Juice::COLA).should eq(false)
+		subject.buyable?(Juice::COLA).should eq false
 	end
 
 	it "在庫に無いジュースは購入不可能" do
 		subject.insert Money::YEN_1000
-		subject.buyable?('sprite').should eq(false)
+		subject.buyable?('sprite').should eq false
 	end
 
 	it "120円投入してコーラが購入できる" do
 		subject.insert Money::YEN_100
 		subject.insert Money::YEN_10
 		subject.insert Money::YEN_10
-		juice = subject.buy('cola')
-		juice.name.should eq('cola')
-		juice.price.should eq(120)
-		subject.get_stock('cola').num.should eq(4)
+		juice = subject.buy 'cola'
+		juice.name.should eq 'cola'
+		juice.price.should eq 120
+		subject.get_stock('cola').num.should eq 4
 	end
 
 	it "50円投入してコーラ購入をしても何も変わらない" do
 		subject.insert Money::YEN_50
-		subject.buy('cola').should eq(nil)
-		subject.get_stock('cola').num.should eq(5)
+		subject.buy('cola').should eq nil
+		subject.get_stock('cola').num.should eq 5
 	end
 
 	it "在庫がない場合に何もしない" do
@@ -199,20 +199,20 @@ describe Jihanki do
 		subject.buy 'cola'
 		subject.buy 'cola'
 		subject.buy 'cola'
-		subject.buyable?('cola').should eq(false)
-		subject.buy('cola').should eq(nil)
-		subject.get_stock('cola').num.should eq(0)
+		subject.buyable?('cola').should eq false
+		subject.buy('cola').should eq nil
+		subject.get_stock('cola').num.should eq 0
 
-		subject.buy('cola').should eq(nil)
-		subject.get_stock('cola').num.should eq(0)
+		subject.buy('cola').should eq nil
+		subject.get_stock('cola').num.should eq 0
 	end
 
 	it "コーラを一本買ったら売上金額120円を取得できる" do
 		subject.insert Money::YEN_1000
 		subject.buy 'cola'
-		subject.get_sales.should eq(120)
-		subject.payback.should eq(880)
-		subject.get_total.should eq(0)
+		subject.get_sales.should eq 120
+		subject.payback.should eq 880
+		subject.get_total.should eq 0
 	end
 
 end
